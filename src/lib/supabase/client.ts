a50e9af6e9457d98a0fbf7b295dfe3379ab61dc5
Subham Mani <manisubham09@gmail.com>
2025-03-24 20:49:34 +0530

    refactor Supabase client initialization for client-side operations

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,5 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Define a function to create a Supabase client for client-side operations
+export const supabaseClient = () =>
+  createBrowserClient(
+    // Pass Supabase URL and anonymous key from the environment to the client
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!
+  );
