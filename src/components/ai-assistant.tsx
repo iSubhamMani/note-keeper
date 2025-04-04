46c7d55f14869b36348b7d0f654589cfc5884c27
Subham Mani <manisubham09@gmail.com>
2025-04-04 11:32:09 +0530

    update

--- src/components/ai-assistant.tsx
+++ src/components/ai-assistant.tsx
@@ -47,7 +47,6 @@ export default function AiAssistant() {
   };
 
   useEffect(() => {
-    console.log("Messages changed");
     bottomRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import { signOut } from "@/actions/auth";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const LogoutButton = () => {
   const router = useRouter();
@@ -12,7 +13,15 @@ const LogoutButton = () => {
         const res = await signOut();
 
         if (res.error) {
-          console.log("error signout out");
+          toast("Error signing out", {
+            duration: 3000,
+            position: "bottom-center",
+            style: {
+              backgroundColor: "#FF3B30",
+              color: "#fff",
+              fontWeight: "bold",
+            },
+          });
           return;
         }
 
